aa835e4a830b5bb86c5d241a4f28c17a8c4c7190
Jayakrishnan Sasi <jayakrishnan.sasi@nyumc.org>
2018-04-06 17:48:12 +0530

    edit/delete/cancel implemented

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './assets/scss/app.scss';
 import Backbone from 'Backbone';
 import $ from 'jQuery';
 import _ from 'underscore';
+import { setTimeout } from 'timers';
 
 //Backbone model
 
@@ -44,6 +45,38 @@ var BlogView =  Backbone.View.extend({
     initialize: function(){
         this.template = _.template($('.blogs-list-template').html());
     },
+    events: {
+        'click .edit-blog' : 'edit',
+        'click .update-blog': 'update',
+        'click .cancel-blog' : 'cancel',
+        'click .delete-blog' : 'delete'
+    },
+    edit: function(){
+        this.$('.edit-blog').hide();
+        this.$('.delete-blog').hide();
+        this.$('.update-blog').show();
+        this.$('.cancel-blog').show();
+
+        var author = this.$(".author").html();
+        var title = this.$(".title").html();
+        var url = this.$(".url").html();3
+
+        this.$(".author").html("<input type='text' class='form-control author-update' value="+ author +">");
+        this.$(".title").html("<input type='text' class='form-control title-update' value="+ title +">");
+        this.$(".url").html("<input type='text' class='form-control url-update' value="+ url +">");
+        
+    },
+    update : function() {
+        this.model.set("author",$(".author-update").val());
+        this.model.set("title",$(".title-update").val());
+        this.model.set("url",$(".url-update").val());
+    },
+    cancel : function () {
+        blogsView.render();
+    },
+    delete : function () {
+        this.model.destroy();
+    },
     render : function(){
         this.$el.html(this.template(this.model.toJSON()));
         return this;
@@ -55,7 +88,14 @@ var BlogsView =  Backbone.View.extend({
     model: blogs,
     el: $('.blogs-list'),
     initialize: function(){
+        var self = this;
         this.model.on('add',this.render,this);
+        this.model.on('change',function(){
+            setTimeout(function(){
+                self.render();
+            },30)
+        },this);
+        this.model.on('remove', this.render, this)
     },
     render: function(){
         var self = this;
@@ -77,6 +117,9 @@ $(function(){
             title: $('.title-input').val(),
             url: $('.url-input').val()
         });
+        $('.author-input').val('');
+        $('.title-input').val('');
+        $('.url-input').val('');
         blogs.add(blog);
     })
 })
